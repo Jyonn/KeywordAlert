9007c1b2554fec5f826b04937c3e3c29c5d043a2
Kola <372529797@qq.com>
2017-07-07 19:19:28 +0800

    try to debug

--- cp_hint/wsgi.py
+++ cp_hint/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import time
+import trackback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cp_hint.settings")
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except:
+    print 'handing WSGI exception'
+
+    if 'mod_wsgi' in sys.modules:
+        trackback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
